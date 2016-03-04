a12afe9ab85d831a121928c59cad9fda8a178a2b
ClemSpy <spy_c@hotmail.fr>
2016-03-04 11:57:33 -0500

    Update tp2.pl

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -1,16 +1,6 @@
 % Auteur: Clément Spies
 % Date: 02/03/2016
 
-% Début de la partie : Initialisation de la partie %
-
-%pion(1,1,vide). pion(2,1,vide). pion(3,1,vide). pion(4,1,vide). pion(5,1,vide). pion(6,1,vide). pion(7,1,vide).
-%pion(1,2,vide). pion(2,2,vide). pion(3,2,vide). pion(4,2,vide). pion(5,2,vide). pion(6,2,vide). pion(7,2,vide).
-%pion(1,3,vide). pion(2,3,vide). pion(3,3,vide). pion(4,3,vide). pion(5,3,vide). pion(6,3,vide). pion(7,3,vide).
-%pion(1,4,vide). pion(2,4,vide). pion(3,4,vide). pion(4,4,vide). pion(5,4,vide). pion(6,4,vide). pion(7,4,vide).
-%pion(1,5,vide). pion(2,5,vide). pion(3,5,vide). pion(4,5,vide). pion(5,5,vide). pion(6,5,vide). pion(7,5,vide).
-%pion(1,6,vide). pion(2,6,vide). pion(3,6,vide). pion(4,6,vide). pion(5,6,vide). pion(6,6,vide). pion(7,6,vide).
-
-
 joueur(max).
 joueur(min).
 
@@ -30,7 +20,6 @@ gagne(J) :- pion(X,Y,J) , X2 is X+1, pion(X2,Y,J), X3 is X+2, pion(X3,Y,J), X4 i
             pion(XA,YA,J) , YA2 is YA+1, pion(XA,YA2,J), YA3 is YA+2, pion(XA,YA3,J), YA4 is YA+3, pion(XA,YA4,J), joueur(J);                                               % 4 en colonne
             pion(XB,YB,J) , YB2 is YB+1, XB2 is XB+1, pion(XB2,YB2,J) , YB3 is YB+2, XB3 is XB+2, pion(XB3,YB3,J) , YB4 is YB+3, XB4 is XB+3, pion(XB4,YB4,J), joueur(J);   % 4 en diagonale
             pion(XC,YC,J) , YC2 is YC-1, XC2 is XC+1, pion(XC2,YC2,J) , YC3 is YC-2, XC3 is XC+2, pion(XC3,YC3,J) , YC4 is YC-3, XC4 is XC+3, pion(XC4,YC4,J), joueur(J).   % 4 en diagonale
-
 
 fin_partie(J) :- gagne(J) ; plein(1),plein(2),plein(3),plein(4),plein(5),plein(6),plein(7).
 
