89b3a50c4fa01deca27d65bd21e9f55c1baf1fbd
ClemSpy <spy_c@hotmail.fr>
2016-03-04 11:40:23 -0500

    Update tp2.pl

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -19,7 +19,7 @@ case(X,Y) :- X > 0, X < 8, Y > 0, Y < 7.
 plein(X) :- pion(X,6,J), joueur(J).
 
 peut_jouer(X,1) :- not(plein(X)) , case(X,1) , not(pion(X,1,_)).
-peut_jouer(X,Y) :- not(plein(X)) , case(X,Y) , not(pion(X,Y,_)) , pion(X,Y-1,_).
+peut_jouer(X,Y) :- not(plein(X)) , case(X,Y) , not(pion(X,Y,_)) , Y2 is Y-1, pion(X,Y2,_).
 
 pion(1,1,min).
 pion(2,1,min).
