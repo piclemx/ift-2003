22c3d82ead27e2c8d14f0e054c70f96e1357ca5e
ClemSpy <spy_c@hotmail.fr>
2016-03-07 21:17:32 -0500

    Update tp2.pl

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -26,9 +26,9 @@ pion(1,3,max).
 pion(1,4,max).
 
 gagne(J) :- pion(X,Y,J) , X2 is X+1, pion(X2,Y,J), X3 is X+2, pion(X3,Y,J), X4 is X+3, pion(X4,Y,J), joueur(J);                                                             % 4 en ligne
-            pion(XA,YA,J) , YA2 is YA+1, pion(XA,YA2,J), YA3 is YA+2, pion(XA,YA3,J), YA4 is YA+3, pion(XA,YA4,J), joueur(J);                                               % 4 en colonne
-            pion(XB,YB,J) , YB2 is YB+1, XB2 is XB+1, pion(XB2,YB2,J) , YB3 is YB+2, XB3 is XB+2, pion(XB3,YB3,J) , YB4 is YB+3, XB4 is XB+3, pion(XB4,YB4,J), joueur(J);   % 4 en diagonale
-            pion(XC,YC,J) , YC2 is YC-1, XC2 is XC+1, pion(XC2,YC2,J) , YC3 is YC-2, XC3 is XC+2, pion(XC3,YC3,J) , YC4 is YC-3, XC4 is XC+3, pion(XC4,YC4,J), joueur(J).   % 4 en diagonale
+            pion(X,Y,J) , Y2 is Y+1, pion(X,Y2,J), Y3 is Y+2, pion(X,Y3,J), Y4 is Y+3, pion(X,Y4,J), joueur(J);                                               % 4 en colonne
+            pion(X,Y,J) , Y2 is Y+1, X2 is X+1, pion(X2,Y2,J) , Y3 is Y+2, X3 is X+2, pion(X3,Y3,J) , Y4 is Y+3, X4 is X+3, pion(X4,Y4,J), joueur(J);   % 4 en diagonale
+            pion(X,Y,J) , Y2 is Y-1, X2 is X+1, pion(X2,Y2,J) , Y3 is Y-2, X3 is X+2, pion(X3,Y3,J) , Y4 is Y-3, X4 is X+3, pion(X4,Y4,J), joueur(J).   % 4 en diagonale
 
 
 fin_partie(J) :- gagne(J) ; plein(1),plein(2),plein(3),plein(4),plein(5),plein(6),plein(7).
@@ -44,6 +44,9 @@ peut_gagner(A,B,J) :-
                   % 3 en diagonale
                   pion(X,Y,J) , Y2 is Y+1, X2 is X+1, pion(X2,Y2,J) , Y3 is Y+2, X3 is X+2, pion(X3,Y3,J), joueur(J), ((A is X-1, B is Y-1, peut_jouer(A,B)) ; (A is X+3, B is Y+3, peut_jouer(A,B)));
                   pion(X,Y,J) , Y2 is Y-1, X2 is X+1, pion(X2,Y2,J) , Y3 is Y-2, X3 is X+2, pion(X3,Y3,J), joueur(J), ((A is X-1, B is Y+1, peut_jouer(A,B)) ;  (A is X+3, B is Y-3, peut_jouer(A,B))).
+
+
+% AFFICHER
 
 afpion(X,Y) :- not(pion(X,Y,_)) , write(' ') ;
                pion(X,Y,min) , write('o') ;
