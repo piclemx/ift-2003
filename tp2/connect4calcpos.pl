17ad011750665be8c80b64b8ff7abb2e867e130c
vincent.beaudoin <vincent.beaudoin@live.ca>
2016-03-09 18:28:43 -0500

    add test by vincent

---
+++ tp2/connect4calcpos.pl
@@ -0,0 +1,88 @@
+% minimax(Pos, BestNextPos, Val)
+% Pos is a position, Val is its minimax value.
+% Best move from Pos leads to position BestNextPos.
+minimax(Pos, BestNextPos, Val) :-                     % Pos has successors
+    bagof(NextPos, move(Pos, NextPos), NextPosList),
+    best(NextPosList, BestNextPos, Val), !.
+
+minimax(Pos, _, Val) :-                     % Pos has no successors
+    utility(Pos, Val).
+
+
+best([Pos], Pos, Val) :-
+    minimax(Pos, _, Val), !.
+
+best([Pos1 | PosList], BestPos, BestVal) :-
+    minimax(Pos1, _, Val1),
+    best(PosList, Pos2, Val2),
+    betterOf(Pos1, Val1, Pos2, Val2, BestPos, BestVal).
+
+
+
+betterOf(Pos0, Val0, _, Val1, Pos0, Val0) :-   % Pos0 better than Pos1
+    min_to_move(Pos0),                         % MIN to move in Pos0
+    Val0 > Val1, !                             % MAX prefers the greater value
+    ;
+    max_to_move(Pos0),                         % MAX to move in Pos0
+    Val0 < Val1, !.                            % MIN prefers the lesser value
+
+betterOf(_, _, Pos1, Val1, Pos1, Val1).        % Otherwise Pos1 better than Pos0
+
+
+
+
+
+
+
+
+
+
+
+
+% move(+Pos, -NextPos)
+% True if there is a legal (according to rules) move from Pos to NextPos.
+move([X1, play, Board], [X2, win, NextBoard]) :-
+	nextPlayer(X1, X2),
+    move_aux(X1, Board, NextBoard),
+    gagne(_, NextBoard, _, X1, _), !.
+
+move([X1, play, Board], [X2, draw, NextBoard]) :-
+	nextPlayer(X1, X2),
+    move_aux(X1, Board, NextBoard),
+    egal(NextBoard), !.
+
+move([X1, play, Board], [X2, play, NextBoard]) :-
+    nextPlayer(X1, X2),
+    move_aux(X1, Board, NextBoard).
+
+% move_aux(+Player, +Board, -NextBoard)
+% True if NextBoard is Board whith an empty case replaced by Player mark.
+move_aux(P, [0|Bs], [P|Bs]).
+
+move_aux(P, [B|Bs], [B|B2s]) :-
+    move_aux(P, Bs, B2s).
+
+
+% min_to_move(+Pos)
+% True if the next player to play is the MIN player.
+min_to_move([o, _, _]).
+
+% max_to_move(+Pos)
+% True if the next player to play is the MAX player.
+max_to_move([x, _, _]).
+
+% utility(+Pos, -Val) :-
+% True if Val the the result of the evaluation function at Pos.
+% We will only evaluate for final position.
+% So we will only have MAX win, MIN win or draw.
+% We will use  1 when MAX win
+%             -1 when MIN win
+%              0 otherwise.
+utility([o, win, _], 1).       % Previous player (MAX) has win.
+utility([x, win, _], -1).      % Previous player (MIN) has win.
+utility([_, draw, _], 0).
+
+% nextPlayer(X1, X2)
+% True if X2 is the next player to play after X1.
+nextPlayer(o, x).
+nextPlayer(x, o).
