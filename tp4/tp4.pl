ff7d14f446995c4a5c4220290c86295ac2afccc4
vincent.beaudoin <vincent.beaudoin@live.ca>
2016-04-17 15:25:57 -0400

    final

--- tp4/tp4.pl
+++ tp4/tp4.pl
@@ -1,19 +1,16 @@
-% Auteur:
-% Date: 12/04/2016
-
-% Le prÃ©dicat lire/2 lit une chaÃ®ne de caractÃ¨res Chaine entre apostrophes
-% et terminÃ©e par un point.
-% Resultat correspond Ã  la liste des mots contenus dans la phrase.
-% Les signes de ponctuation ne sont pas gÃ©rÃ©s.
+% Le prédicat lire/2 lit une chaîne de caractères Chaine entre apostrophes
+% et terminée par un point.
+% Resultat correspond à la liste des mots contenus dans la phrase.
+% Les signes de ponctuation ne sont pas gérés.
 lire(Chaine,Resultat):- write('Entrer votre question (entre apostrophes, en minuscule, sans ponctuation) : '), read(Chaine),
         name(Chaine, Temp), chaine_liste(Temp, Resultat),!.
 
-% PrÃ©dicat de transformation de chaÃ®ne en liste
+% Prédicat de transformation de chaîne en liste
 chaine_liste([],[]).
 chaine_liste(Liste,[Mot|Reste]):- separer(Liste,32,A,B), name(Mot,A),
         chaine_liste(B,Reste).
 
-% SÃ©pare une liste par rapport Ã  un Ã©lÃ©ment
+% Sépare une liste par rapport à un élément
 separer([],_,[],[]):-!.
 separer([X|R],X,[],R):-!.
 separer([A|R],X,[A|Av],Ap):- X\==A, !, separer(R,X,Av,Ap).
@@ -25,19 +22,19 @@ lancer() :-
         reponse(Q, _).
 
 % Base d'informations
-cout(quÃ©bec, montrÃ©al, '50 $').
-cout(quÃ©bec, newyork, '200 $').
-trajet(quÃ©bec, montrÃ©al, '16h00', '21h30').
-trajet(quÃ©bec, newyork, '0h00', '12h30').
+cout(québec, montréal, '50 $').
+cout(québec, newyork, '200 $').
+trajet(québec, montréal, '16h00', '21h30').
+trajet(québec, newyork, '0h00', '12h30').
 trajet(detroit, newyork, '0h00', '12h30').
-trajet(quÃ©bec, detroit, '19h00', '23h00').
-trajet(montrÃ©al, detroit, '12h00', '20h00').
-temps(quÃ©bec, montrÃ©al, '5h30').
-temps(quÃ©bec, newyork, '12h30').
-pauses(quÃ©bec, montrÃ©al, 0).
-pauses(quÃ©bec, newyork, 2).
+trajet(québec, detroit, '19h00', '23h00').
+trajet(montréal, detroit, '12h00', '20h00').
+temps(québec, montréal, '5h30').
+temps(québec, newyork, '12h30').
+pauses(québec, montréal, 0).
+pauses(québec, newyork, 2).
 
-% Analyse sÃ©mantique
+% Analyse sémantique
 question( SEM ) --> mq, gv(ACT, _), prep, ville(NOM1), conj, ville(NOM2), { SEM = [ACT, NOM1, NOM2, _] }.
 question( SEM ) --> prep, mq, nc(_), gv(_, ACT), prep, ville(NOM1), prep, ville(NOM2), { SEM = [ACT, NOM1, NOM2, _, _] }.
 question( SEM ) --> mq, prep, nc(ACT), gv(_, _), prep, ville(NOM1), conj, ville(NOM2), { SEM = [ACT, NOM1, NOM2, _] }.
@@ -58,13 +55,13 @@ mq --> [quelles].
 
 pro --> [qui].
 
-v( cout ) --> [coÃ»te].
+v( cout ) --> [coûte].
 v( part ) --> [est].
 v( dure ) --> [dure].
 v( prendra ) --> [prendra].
 v( sont ) --> [sont].
 v( partent ) --> [partent].
-v( mÃ¨nent ) --> [mÃ¨nent].
+v( mènent ) --> [mènent].
 
 
 art --> [un].
@@ -78,18 +75,18 @@ nc( temps ) --> [temps].
 nc( fois ) --> [fois].
 nc( chauffeur ) --> [chauffeur].
 nc( pauses ) --> [pauses].
-nc( trajet ) --> [dÃ©part].
+nc( trajet ) --> [départ].
 nc( villes ) --> [villes].
 
 prep --> [entre].
-prep --> [Ã ].
+prep --> [à].
 prep --> [de].
 prep --> [pour].
 
 adj --> [prochain].
 
-ville( quÃ©bec ) --> [quÃ©bec].
-ville( montrÃ©al ) --> [montrÃ©al].
+ville( québec ) --> [québec].
+ville( montréal ) --> [montréal].
 ville( newyork ) --> [newyork].
 ville( detroit ) --> [detroit].
 
@@ -97,8 +94,8 @@ ville( detroit ) --> [detroit].
 
 conj --> [et].
 
-% RÃ©ponse aux questions
-reponse([ACT, NOM1, NOM2, REPONSE], REPONSE) :- call(ACT, NOM1, NOM2, REPONSE), !, write('La rÃ©ponse est : '), write(REPONSE).
-reponse([ACT, NOM1, NOM2, X, Y], REPONSE) :- call(ACT, NOM1, NOM2, X, Y), !, atom_concat(X, ' - ', Z), atom_concat(Z, Y, REPONSE), write('La rÃ©ponse est : '), write(REPONSE).
-reponse([AGNT, NOM], REPONSE) :- findall(DEST,call(AGNT, NOM, DEST , _, _),REPONSE), !, atomic_list_concat(REPONSE, ', ', REPONSE1) , write('La rÃ©ponse est : '), write(REPONSE1).
-reponse([AGNT, DEST], REPONSE) :- findall(DEP,call(AGNT, DEP, DEST , _, _),REPONSE), !, atomic_list_concat(REPONSE, ', ', REPONSE1) , write('La rÃ©ponse est : '), write(REPONSE1).
+% Réponse aux questions
+reponse([ACT, NOM1, NOM2, REPONSE], REPONSE) :- call(ACT, NOM1, NOM2, REPONSE), !, write('La réponse est : '), write(REPONSE).
+reponse([ACT, NOM1, NOM2, X, Y], REPONSE) :- call(ACT, NOM1, NOM2, X, Y), !, atom_concat(X, ' - ', Z), atom_concat(Z, Y, REPONSE), write('La réponse est : '), write(REPONSE).
+reponse([AGNT, NOM], REPONSE) :- findall(DEST,call(AGNT, NOM, DEST , _, _),REPONSE), !, atomic_list_concat(REPONSE, ', ', REPONSE1) , write('La réponse est : '), write(REPONSE1).
+reponse([AGNT, DEST], REPONSE) :- findall(DEP,call(AGNT, DEP, DEST , _, _),REPONSE), !, atomic_list_concat(REPONSE, ', ', REPONSE1) , write('La réponse est : '), write(REPONSE1).
